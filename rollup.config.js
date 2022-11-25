037b3b33b02f481477a89f8570927ca441a05736
nowm1006 <nowm1006@yahoo.co.jp>
2022-11-25 13:19:50 +0900

    added tslib added scripts added rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+    imput: 'src/main.ts',
+    output: {
+        file: 'dist/main.js',
+        format: 'iife'
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript(),
+        terser()
+    ]
+}
