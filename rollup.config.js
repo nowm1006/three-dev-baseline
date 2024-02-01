47b6607d1cf0adda139fe6fc690fd70a9ecd0bd7
nowm1006 <78073033+nowm1006@users.noreply.github.com>
2024-02-01 05:15:18 +0000

    added rollup-plugin-url

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 import css from 'rollup-plugin-css-only'
+import url from '@rollup/plugin-url'
 
 export default {
     input: 'src/main.ts',
@@ -15,6 +16,7 @@ export default {
         commonjs(),
         typescript(),
         css({output:'style.css'}),
-        terser()
+        terser(),
+        url()
     ]
 }
